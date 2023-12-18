7898ed8162bc23622790e247fb4ec1bda4f508ad
Lucas Hazardous <70599201+LucasHazardous@users.noreply.github.com>
2023-12-18 20:55:47 +0000

    copilot third commit again

--- comments.js
+++ comments.js
@@ -1,31 +1,43 @@
-// create web server
-// start server: node comments.js
-// test in browser: http://localhost:3000
+// Create web server
+var express = require('express')
+var app = express()
+var fs = require('fs')
+var bodyParser = require('body-parser')
 
-var http = require('http');
-var url = require('url');
-var items = [];
+// Use body parser
+app.use(bodyParser.urlencoded({ extended: false }))
 
-var server = http.createServer(function(req, res){
-	switch (req.method) {
-		case 'POST':
-			var item = '';
-			req.setEncoding('utf8');
-			req.on('data', function(chunk){
-				item += chunk;
-			});
-			req.on('end', function(){
-				items.push(item);
-				res.end('OK\n');
-			});
-			break;
-		case 'GET':
-			items.forEach(function(item, i){
-				res.write(i + ') ' + item + '\n');
-			});
-			res.end();
-			break;
-	}
-});
+// Set view engine
+app.set('view engine', 'ejs')
 
-server.listen(3000);
+// Set static folder
+app.use(express.static('public'))
+
+// Get data from json file
+var data = fs.readFileSync('comments.json')
+var comments = JSON.parse(data)
+
+// Get data from json file
+var data = fs.readFileSync('comments.json')
+var comments = JSON.parse(data)
+
+// Get home page
+app.get('/', function (req, res) {
+  res.render('index', { comments: comments })
+})
+
+// Post data
+app.post('/addComment', function (req, res) {
+  var newComment = {
+    name: req.body.name,
+    comment: req.body.comment
+  }
+  comments.push(newComment)
+  fs.writeFileSync('comments.json', JSON.stringify(comments))
+  res.redirect('/')
+})
+
+// Listen on port 3000
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
